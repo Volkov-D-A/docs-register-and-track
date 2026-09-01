9b222e71cb0d7f2f7ef932108a8a78bad8d6de7f
Volkov-D-A <dimas.volkov@gmail.com>
2026-09-01 16:41:21 +0500

    runtime artifacts

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1941,6 +1941,47 @@ export namespace models {
 		    return a;
 		}
 	}
+	export class Attachment {
+	    filename: string;
+	    filepath: string;
+	    fileSize: number;
+	    contentType: string;
+	    uploadedByName?: string;
+	    // Go type: time
+	    uploadedAt: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new Attachment(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.filename = source["filename"];
+	        this.filepath = source["filepath"];
+	        this.fileSize = source["fileSize"];
+	        this.contentType = source["contentType"];
+	        this.uploadedByName = source["uploadedByName"];
+	        this.uploadedAt = this.convertValues(source["uploadedAt"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class AttachmentStorageReconciliation {
 	    missingObjects: string[];
 	    orphanObjects: string[];
